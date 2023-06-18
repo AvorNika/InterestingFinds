a2b842d47d25ce69ef85e66a2db2a8bda14da826
viktoriya <shakirova.vlg@gmail.com>
2023-06-18 14:41:12 +0500

    Delete 7 files, add binary sort

--- 125. ValidPalindrome(leetcode).py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        new_s = []
-        for sym in s.lower():
-            if sym in 'abcdefghijklmnopqrstuvwxyz0123456789':
-                new_s.append(sym)
-
-        return new_s == new_s[::-1]
--- 217. ContainsDuplicate(leetcode).py
+++
@@ -1,9 +0,0 @@
-user_nums = [int(num) for num in input().split()]
-
-
-def containsDuplicate(nums):
-    new_nums = set(nums)
-    return len(nums) != len(new_nums)
-
-
-print(containsDuplicate(user_nums))
